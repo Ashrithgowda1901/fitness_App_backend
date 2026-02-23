173487e1ef6e10708c5c5b5f0c30ccb8a2e4f46e
Ashrithgowda1901 <ashrithgowda04@gmail.com>
2026-02-23 23:56:18 +0530

    added optional auth for /me

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,12 @@
 const express=require("express");
 const { registerUser, loginUser, logout } = require("../controller/auth.controller");
 const router=express.Router();
-const authMiddleware=require("../middlewares/auth.middleware")
+const authMiddleware=require("../middlewares/auth.middleware");
+const optionAuthMiddle=require("../middlewares/optionalAuth.middleware");
 
 router.post("/register",registerUser);
 router.post("/login",loginUser);
-router.get("/me",authMiddleware,(req,res)=>{
+router.get("/me",optionAuthMiddle,(req,res)=>{
     return res.status(200).json({
         user:req.user
     });
